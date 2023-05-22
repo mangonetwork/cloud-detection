98be42d5cad5f53c12476ddafaf9ec45e2546e68
MatthewBurnes <mattburnes02@gmail.com>
2023-05-22 16:19:57 -0700

    still trying to fix circle image, but otherwise good progress

--- GetVals.py
+++ GetVals.py
@@ -2,10 +2,10 @@
 """
 Created on Wed Mar  8 16:35:03 2023
 
-@author: ndfon
+@author: ndfon & MBurnes
 """
 import requests as rqs #urls from website
-import matplotlib as mp
+#import matplotlib as mp
 import matplotlib.pyplot as plt
 import pandas as pd
 
@@ -13,9 +13,10 @@ import numpy as np
 import h5py as h5 # hdf5 files - large files compact
 import shutil as sh
 import os as os
-
-def func():
-    url = 'https://data.mangonetwork.org/data/transport/mango/archive/low/greenline/raw/2022/237/05/mango-low-greenline-20220825-052400.hdf5'
+from PIL import Image, ImageDraw
+import math
+def func(url):
+    #url = 'https://data.mangonetwork.org/data/transport/mango/archive/low/greenline/raw/2022/237/05/mango-low-greenline-20220825-052400.hdf5'
     r = rqs.get(url, stream=True)
     dump = r.raw
     cwd = os.getcwd()
@@ -25,21 +26,58 @@ def func():
     file = h5.File('testfile.hdf5', 'r')
     
     a = file['image'] #array of values
+    emptA = []
 
-    a = a[100:450, 100:550]
+    #a = a[100:450, 100:550]
+    h = len(a[0])
+    k = len(a[:, 0])
+    count = 0
+    #radius to use for equation of a circle: 
+    rad = 50
+    #image has decimals, need to iterate with for all and then use .iO for finding temporary dimensions for the circle calculation
+    for x in range(k):
+        for y in range(h):
+            circleCalc = (x - (k/2))**2 + (y - (h/2))**2 
+            # print('h' + str(h))
+            # print(k)
+            # print(rad**2)
+            # print(circleCalc)
+            if circleCalc > rad**2:
+                a[x][y] = 10000
+                count += 1
+                
+
+
+    # for i in range(w):
+    #     for j in range(h):
+    #         if (i)
+
+    # lum_img = Image.new('L',[h,w] ,0)
+    # draw = ImageDraw.Draw(lum_img)
+    # draw.pieslice([(0,0),(h,w)],0,360,fill=255)
+    # img_arr = np.array(a)
+    # lum_img_arr = np.array(lum_img)
+
+
+    #make something to trim the rectangle into a central circle. get all arrays that are in positions index <= abs((length/2) + 
     #print(a.shape)
     #print(a[1])
     #print(list(file.keys())) # don't need units, only images
-    #imgplot = plt.imshow(a)
-    #plt.show()
-    #plt.imshow(a, cmap = 'hot')
-    #plt.show()
+    
+    # final_img_arr = np.dstack((img_arr, lum_img_arr))
+    # imgplot = plt.imshow(Image.fromarray(final_img_arr))
+    # plt.show()
+    print(count)
+    print(k, h)
+    plt.imshow(a, cmap = 'hot')
+    plt.show()
+    print(a)
+    
+    
     #b=[]
     #for i in a:
         #b += [x for x in i if x < 60000]
-    print(np.percentile(a, 90))
-    print(np.percentile(a, 10))
-    print(np.median(a))
-func()
+    return [np.median(a), np.percentile(a, 90),  np.percentile(a, 10)]
+#func()
 
 #def func1():
---
+++ automatedDataEntry.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+#import seaborn as sns
+#import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+import getUrls
+import GetVals
+import csv
+
+
+urlList = getUrls.getURL()
+for lst in urlList:
+    letter = lst[1]
+    #print(lst[0])
+    vals = GetVals.func(lst[0])
+    lst[1] = vals[0]
+    lst.append(vals[1])
+    lst.append(vals[2])
+    lst.append(letter)
+    # with open('Brightness_Data_Copy.csv', 'w', newline='') as myfile:
+    #     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+    #     wr.writerow(lst)
+
+
+df = pd.DataFrame(urlList, columns=['URL', 'Median', '90th Percentile', '10th Percentile', 'ClearSky'])
+df.to_csv("./Brightness_Data_Copy.csv", sep = ',', index = False)
+
+
+data = pd.read_csv("Brightness_Data_Copy.csv")
+x = data.drop(['ClearSky'], axis=1)
+x = x.drop(['URL'], axis = 1)
+y = data['ClearSky']
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.4, random_state=3)
+k_range = list(range(1,26))
+scores = []
+for k in k_range:
+    knn = KNeighborsClassifier(n_neighbors=k)
+    knn.fit(x_train, y_train)
+    y_pred = knn.predict(x_test)
+    scores.append(metrics.accuracy_score(y_test, y_pred))
+# plt.figure()
+# plt.plot(k_range, scores, 'k.-')
+# plt.show()
+knn = KNeighborsClassifier(n_neighbors=5)
+knn.fit(x, y)
+u = knn.predict([[4733, 3564.1, 3917]])
+print(u)
