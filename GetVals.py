Fixed Cropping, accuracy up to 91.58%, precision at 90.21%

GetVals.py
@@ -31,7 +31,7 @@ def func(bigDF, url='', arr=[[]]):
         arr = file['image'] #array of values
 
     
-    a = arr[100:450, 100:550] #crop the image
+    a = arr[50:450, 100:600]  #crop the image
     
     h = len(a[0])
     k = len(a[:, 0])
@@ -49,6 +49,7 @@ def func(bigDF, url='', arr=[[]]):
                 count += 1
                 
     norm = np.linalg.norm(a) #the data in the image is normalized before the Fourier transformation
+    pre_norm_a = a
     a = a/norm 
     d = scipy.fft.fft2(a) #The Fourier features are calculated through the fourier transform and then abs is used to combine the real and imaginary components
     for i in range(len(d)):
@@ -58,10 +59,13 @@ def func(bigDF, url='', arr=[[]]):
     d = d.real #we use d.real to dispose of the 0j value still present after using abs
     bigDF[url] = np.ndarray.flatten(d) 
     #print(len(d[0]))
+    a = a * norm
     #updated: 1, 450, 453, 7199 are the flattened fourier features I chose. Space for one more (8549 should be used if desired)
-    return [np.mean(dataPoints), d[0][1], d[1][0], d[19][0], d[15][449], np.std(dataPoints)] #mean of datapoints and the most significant fourier features are returned, greenline
+    return [np.mean(pre_norm_a), d[0][1], d[1][0], d[1][499], d[399][496]] # replaced d[0][1], d[1][0], d[19][0] d[15][449] #mean of datapoints and the most significant fourier features are returned, greenline
     #after redline integration, 1, 449, 450, 157040, 7649, 8549, 149401, 150301
     #return [np.mean(dataPoints), d[0][5], d[63][425], d[39][128], d[311][322]] #redline #
+    #no signiciant difference for circle crop vs rectangle crop (no var)
+    #
 
 
 #453

automatedDataEntry.py
@@ -38,44 +38,69 @@ bigDF = pd.DataFrame()
 ##SECTION 1: USING GETVALS.PY TO ACQUIRE ALL THE NECESSARY DATA. UNCOMMENT THIS SECTION TO RECOLLECT DATA
 
 #       this for loop is what collects the data, iteraing through GETURLS.py which is a list of the desired images
-for lst in urlList:
+# for lst in urlList:
     
-    letter = lst[1]
-    #print(lst[0])
+#     letter = lst[1]
+#     #print(lst[0])
 
-        #vals is the function that analyses each image and calculates each relevant feature
+#         #vals is the function that analyses each image and calculates each relevant feature
     
-    vals = GetVals.func(bigDF, lst[0])
-    #print(vals)
-    lst[1] = vals[0]
-    lst.append(vals[1])
-    lst.append(vals[2])
-    lst.append(vals[3])
-    lst.append(vals[4])
-    lst.append(vals[5])
-    lst.append(letter)
-    #print(lst)
-       # this block writes the results to the brightness data csv file where all relevant stats are kept
-    with open('Brightness_Data_Copy.csv', 'w', newline='') as myfile:
-        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
-        wr.writerow(lst)
-newList = []
+#     vals = GetVals.func(bigDF, lst[0])
+#     #print(vals)
+#     lst[1] = vals[0]
+#     lst.append(vals[1])
+#     lst.append(vals[2])
+#     lst.append(vals[3])
+#     lst.append(vals[4])
+#     lst.append(letter)
+#     #print(lst)
+#        # this block writes the results to the brightness data csv file where all relevant stats are kept
+#     with open('Brightness_Data_Copy.csv', 'w', newline='') as myfile:
+#         wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+#         wr.writerow(lst)
+# # newList = []
 
-df = pd.DataFrame(urlList, columns=['URL', 'Mean', 'FF1', 'FF2', 'FF3', 'FF4', 'Std_Dev', 'ClearSky'])
-df.to_csv("./Brightness_Data_Copy.csv", sep = ',', index = False)
+# df = pd.DataFrame(urlList, columns=['URL', 'Mean', 'FF1', 'FF2', 'FF3', 'FF4', 'ClearSky'])
+# df.to_csv("./Brightness_Data_Copy.csv", sep = ',', index = False)
 
 
 ##END OF SECTION 1
 
 #THIS PART DISPLAYS A SELECT FOURIER FEATURE AS A PATTERN
-# lst = np.zeros((350, 450))
-# lst[349, 3] = 1
+# lst = np.zeros((400, 500))
+# lst[0, 1] = 1
 # inverted = scipy.fft.ifft2(lst)
-# #inverted = abs(inverted)
 # inverted =  inverted.real
 # plt.imshow(inverted)
+# fig, axs = plt.subplots(2, 2)
+# axs[0, 0].imshow(inverted)
+# axs[0, 0].axis("off")
+# axs[0, 0].set_title('Fourier Feature [0][1]')
+# lst = np.zeros((400, 500))
+# lst[1, 0] = 1
+# inverted = scipy.fft.ifft2(lst)
+# inverted =  inverted.real
+# axs[0, 1].imshow(inverted)
+# axs[0, 1].axis("off")
+# axs[0, 1].set_title('Fourier Feature [1][0]')
+# lst = np.zeros((400, 500))
+# lst[1, 449] = 1
+# inverted = scipy.fft.ifft2(lst)
+# inverted =  inverted.real
+# axs[1, 0].imshow(inverted)
+# axs[1, 0].axis("off")
+# axs[1, 0].set_title('Fourier Feature [1][499]')
+# lst = np.zeros((400, 500))
+# lst[399, 496] = 1
+# inverted = scipy.fft.ifft2(lst)
+# inverted =  inverted.real
+# axs[1, 1].imshow(inverted)
+# axs[1, 1].axis("off")
+# axs[1, 1].set_title('Fourier Feature [399][496]')
+# fig.tight_layout()
 # plt.show()
-
+# exit()
+#d[0][1], d[1][0], d[1][499], d[399][496]
 
 
 
@@ -106,23 +131,54 @@ yTest = dataTest['ClearSky'] #save the classification column as a variable
 #         newList.append(0)
 #     else:
 #         newList.append(1)
-
+# print(newList)
 # res =[]
 # indicies = []
+# neg_res = []
+# neg_indicies = []
 # for d in range(len(bigDF.index)):
 #     b = bigDF.iloc[d].to_numpy()
 #     #print(b)
 #     #print(np.corrcoef(newList, b))
-#     if np.corrcoef(newList, b)[0][1] > 0.55:
+    
+#     if np.corrcoef(newList, b)[0][1] > 0.50:
 #         print(d)
 #         indicies.append(d)
 #         print(np.corrcoef(newList, b)[0][1])
 #         res.append(np.corrcoef(newList, b)[0][1])
-# print(res.index(min(res)))
+    
+#     if np.corrcoef(newList, b)[0][1] < -0.50:
+#         print(d)
+#         neg_indicies.append(d)
+#         print(np.corrcoef(newList, b)[0][1])
+#         neg_res.append(np.corrcoef(newList, b)[0][1])
 # print(res) #All the following dated sept 23: after data pruning, best features are 1/449 at -0.69089 and 449/157050 at -0.6450 for negatives. There are a ton of positives > 0.55, but use those only if necessary. For positives >0.6, we have 453/157494 at 0.6310, 7199/150751 at 0.60578, and 8549/150751 at 0.6044
 # print(indicies) #1, 449, 450, 157050 for <= -0.55, 450 = [1][0] of the fourier transform, 157050 = [349][0]. For >= 0.5, we have 453, 7199 at .5449505 and .5302 (.53019) 8549 at 0.5300 (.5296682). Two indicators from < -0.55, is 1 at -0.58547 and 450/157050 at -0.59064
 max_score = 0.0
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=4/10, random_state=68)
+logr = LogisticRegression(max_iter=10000)
+logr.fit(x_train, y_train)
+y_pred = logr.predict(x_test)
+curr_score = metrics.accuracy_score(y_test, y_pred)
+curr_p = metrics.precision_score(y_test, y_pred, pos_label='Y')
+print(curr_score)
+print(curr_p)
+plt.rcParams.update({'font.size': 15})
+RocCurveDisplay.from_estimator(logr, x, y).plot()
+
+plt.show()
+cm = confusion_matrix(y, logr.predict(x), normalize='true')
+disp = ConfusionMatrixDisplay(cm, display_labels=['cloudy', 'clear'])
+disp.plot()
+plt.show()
+exit()
+
+# print(neg_indicies)
+# print(neg_res)
+# exit()
 avg = []
+precision_avg = []
 svm_avg = []
 num_iters = 1
 curr_best = 0
@@ -131,7 +187,8 @@ for j in range(1,7):
     svmsumm = 0
     num_iters = 0
     num_iters_SVM = 0
-    for i in range(1, 10000):
+    precision_sum = 0
+    for i in range(1, 1000):
         x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=j/10, random_state=i) #split the dataset into a test/train duo for fitting (60% of data) and evaluating fit quality (40% of data). Consistent random state is used to ensure testing is consistent 
        #k_range = list(range(1,26)) #this list is used to generate a plot of the test accuracy depending on the number of nearest neighbors (k). 25 is chosen as the limit rather arbitrarily, but anything beyond 25 is usually victim to overfitting
         scores = []
@@ -143,8 +200,15 @@ for j in range(1,7):
                 y_count += 1
             elif row == 'N':
                 n_count += 1
-        # if n_count > 160 or y_count > 160:
-        #     continue
+        counted = True
+        if (n_count == ((len(y_train) / 2)) or y_count == ((len(y_train) / 2))) and j == 4:
+            print(i, j)
+            counted = False
+
+            continue
+
+        if not counted:
+            print('bad outcome')
 
         #As an alternative to the KNN 'lazy' prediction, this block tests logistic regression on the same train/test set as knn
         cw = {'Y': 1.0, 'N': 1.0}
@@ -158,6 +222,7 @@ for j in range(1,7):
         y_pred = logr.predict(x_test)
         #y_test_pred = logr.predict(xTest)
         curr_score = metrics.accuracy_score(y_test, y_pred)
+        curr_p = metrics.precision_score(y_test, y_pred, pos_label='Y')
         try:
                 clf = svm.SVC(max_iter=10000, class_weight=cw)
                 clf.fit(x_train, y_train)
@@ -174,6 +239,7 @@ for j in range(1,7):
         #     print(metrics.accuracy_score(y_test, y_pred))
         #     exit()
         summ += curr_score
+        precision_sum += curr_p
         svmsumm += curr_score_SVM
         if curr_score > max_score:
             max_score = curr_score
@@ -182,13 +248,16 @@ for j in range(1,7):
     if num_iters != 0:
         totalAvg = summ/num_iters
         avg.append([totalAvg, j])
+        totalP = precision_sum/num_iters
+        precision_avg.append([totalP, j])
     if num_iters_SVM != 0:
         totalAvg = svmsumm/num_iters_SVM
         svm_avg.append([totalAvg, j])
-#print(max_score)
-#print(curr_best)
-#print(curr_best2)
+print(max_score)
+print(curr_best)
+print(curr_best2)
 print(avg)
+print(precision_avg)
 print(svm_avg)
 print(y.shape)
 print(y_pred.shape)
@@ -216,3 +285,4 @@ disp = ConfusionMatrixDisplay(cm, display_labels=['cloudy', 'clear'])
 disp.plot()
 plt.show()
 
+#currently running 