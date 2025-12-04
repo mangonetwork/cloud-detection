c32415192e9d2d464c6454ed07009ee9747929ff
Leslie Lamarche <leslie.lamarche@sri.com>
2025-12-03 20:56:34 -0800

    Add stationtime function to check_cloudy module.

--- src/mangonetwork/clouddetect/check_cloudy.py
+++ src/mangonetwork/clouddetect/check_cloudy.py
@@ -1,4 +1,6 @@
 # check_cloudy.py
+# Note that this module retuns True for cloudy images adn False for clear images
+# This is the inverse of makePrediction, which returns Y for clear images and N for cloudy imagges
 import os
 import datetime as dt
 import h5py
@@ -20,9 +22,6 @@ def rawfile(filename):
         
     """
 
-    #with h5py.File(filename, 'r') as h5:
-    #    instrument = h5['image'].attrs['instrument']
-
     res = makePrediction(filename=filename)
 
     if res == 'Y':
@@ -31,13 +30,47 @@ def rawfile(filename):
         return True
 
 
+def stationtime(station, instrument, time):
+    """
+    Determine if image is cloudy or not based on a particular camera and time.
+
+    Input
+    -----
+    station : str
+        Three-letter station code
+    instrument: str
+        Instrument (either 'greenline' or 'redline')
+    time : datetime.datetime
+        Time
+    
+    Output
+    ------
+    cloudy : bool
+        True = image is cloudy; False = image is clear
+        
+    """
+
+    raw_filename = f'{station}/{instrument}/raw/{time:%Y/%j/%H}/mango-{station}-{instrument}-{time:%Y%m%d-%H%M%S}.hdf5'
+    base_url = 'https://data.mangonetwork.org/data/transport/mango/archive/'
+    url = base_url+raw_filename
+
+    res = makePrediction(url=url)
+
+    if res == 'Y':
+        return False
+    else:
+        return True
+
 
 def processedfile(filename, raw_mango_dir=None):
     """Check if each image is cloudy or not"""
 
+    print(filename)
+
     with h5py.File(filename) as h5:
-        station = h5["ImageData"].attrs["station"]
-        instrument = h5["ImageData"].attrs["instrument"]
+        print(h5["ImageData"])
+        station = h5["ImageData"].attrs["Station"]
+        instrument = h5["ImageData"].attrs["Instrument"]
         utime = h5["UnixTime"][:,0]
 
     cloudy = list()
